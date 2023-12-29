43e8d79a72aa1f572b601522c7b9a0830c9f9407
Darrel Israel <israeldarrel@yahoo.com>
2023-12-30 04:20:07 +0800

    persistent slider

--- popup.js
+++ popup.js
@@ -1,12 +1,28 @@
-// popup.js
 document.addEventListener('DOMContentLoaded', function () {
-    const volumeSlider = document.getElementById('volume-slider');
-  
-    volumeSlider.addEventListener('input', function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { action: 'setVolume', volume: volumeSlider.value });
-      });
+  const volumeSlider = document.getElementById('volume-slider');
+
+  // Retrieve the stored volume value on extension popup load
+  chrome.storage.sync.get(['volume'], function (result) {
+    const storedVolume = result.volume || 50; // Default to 50% if no value is stored
+    volumeSlider.value = storedVolume;
+
+    // Update the volume on the content script
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, { action: 'setVolume', volume: storedVolume });
     });
   });
-  
+
+  volumeSlider.addEventListener('input', function () {
+    const currentVolume = volumeSlider.value;
+
+    // Save the current volume value to storage
+    chrome.storage.sync.set({ volume: currentVolume });
+
+    // Send the volume value to the content script
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, { action: 'setVolume', volume: currentVolume });
+    });
+  });
+});
